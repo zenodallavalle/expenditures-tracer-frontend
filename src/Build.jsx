ca8aea753440b66a4f2abdb648cff92fbecb6b13
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-12-08 00:34:20 +0100

    closed #7

--- src/Build.jsx
+++ src/Build.jsx
@@ -1,29 +1,29 @@
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
-import { AutoBlurButton } from 'utils';
+import { AutoBlurButton } from "utils";
 
 const Build = () => {
   const history = useHistory();
   const backToApp = () => {
-    history.push('/');
+    history.push("/");
   };
   return (
     <div>
-      <h5 className='text-center'>{process.env.REACT_APP_NAME}</h5>
-      <div className='text-center mb-1'>
-        <span className='me-1'>Running mode:</span>
+      <h5 className="text-center">{process.env.REACT_APP_NAME}</h5>
+      <div className="text-center mb-1">
+        <span className="me-1">Running mode:</span>
         <span>{process.env.NODE_ENV}</span>
       </div>
-      <div className='text-center mb-1'>
-        <span className='me-1'>Build version:</span>
+      <div className="text-center mb-1">
+        <span className="me-1">Build version:</span>
         <span>{process.env.REACT_APP_VERSION}</span>
       </div>
-      <div className='text-center mb-3'>
-        <span className='me-1'>Build date:</span>
-        <span>07-12-2021</span>
+      <div className="text-center mb-3">
+        <span className="me-1">Build date:</span>
+        <span>08-12-2021</span>
       </div>
       <div>
-        <AutoBlurButton className='w-100' onClick={backToApp}>
+        <AutoBlurButton className="w-100" onClick={backToApp}>
           Click here to get back to the app
         </AutoBlurButton>
       </div>
