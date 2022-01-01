2634ccf23876e0a640086c4761e94f6b02945424
Zeno Dalla Valle <zenodallavalle@gmail.com>
2022-01-01 21:54:02 +0100

    close #20

--- src/utils/src/getCurrentMonth.js
+++ src/utils/src/getCurrentMonth.js
@@ -2,7 +2,8 @@ const getCurrentMonth = () => {
   const n = new Date();
   const month = n.getMonth() + 1;
   const year = n.getFullYear();
-  return `${month}-${year}`;
+  const monthString = String('0' + month).slice(-2);
+  return `${monthString}-${year}`;
 };
 
 export default getCurrentMonth;
